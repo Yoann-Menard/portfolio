5c75c43736d8586e1a6ea246b6f06458afcb9ffb
Yoann <yoann.menard@epitech.eu>
2020-08-10 19:46:44 +0200

    Updates

---
+++ precache-manifest.c5cca6b1a5c6a257f48a570b0ffccf98.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cdbcd948101e3b748d80e54b44577e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "426473e056b56d19bbc1",
+    "url": "/portfolio/static/css/main.75eb9290.chunk.css"
+  },
+  {
+    "revision": "2f6eecb2988bcf3a2d40",
+    "url": "/portfolio/static/js/2.0656efc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0656efc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426473e056b56d19bbc1",
+    "url": "/portfolio/static/js/main.3bc11de4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "32525fdaf3ae6970a554d8e054169f69",
+    "url": "/portfolio/static/media/BEP.32525fda.jpg"
+  },
+  {
+    "revision": "dc88c9a53b4322c9859df55a2066a866",
+    "url": "/portfolio/static/media/CV_Menard_Yoann.dc88c9a5.pdf"
+  },
+  {
+    "revision": "20ee145f6afe7d8c10de3a6eac24b172",
+    "url": "/portfolio/static/media/css3.20ee145f.png"
+  },
+  {
+    "revision": "00f08d0fc7938f34ba51bea7df57aa5b",
+    "url": "/portfolio/static/media/donkey-kong-country-2-diddys-kong-quest.00f08d0f.jpg"
+  },
+  {
+    "revision": "895bcfa30cd5c8eb5f84834ba0aca2f9",
+    "url": "/portfolio/static/media/earwig-factory.895bcfa3.ttf"
+  },
+  {
+    "revision": "5fc4dd078b8c17c599051b48cc0fb538",
+    "url": "/portfolio/static/media/ecommerce.5fc4dd07.png"
+  },
+  {
+    "revision": "5416efb3320a34989fe6b5c69681cd77",
+    "url": "/portfolio/static/media/javascript.5416efb3.png"
+  },
+  {
+    "revision": "b0ba6d0d7144a425934b9f2daab2b971",
+    "url": "/portfolio/static/media/laravel.b0ba6d0d.png"
+  },
+  {
+    "revision": "9f3ae8362028fa7d5955121473383629",
+    "url": "/portfolio/static/media/mothers_day.9f3ae836.png"
+  },
+  {
+    "revision": "472857fbcc1ef9a8a6172008c4a3b22a",
+    "url": "/portfolio/static/media/music.472857fb.mp3"
+  },
+  {
+    "revision": "dab5af9dea11894a833faf7f1386da34",
+    "url": "/portfolio/static/media/my_computer.dab5af9d.jpg"
+  },
+  {
+    "revision": "c070c582733b0e20a155283406a11f03",
+    "url": "/portfolio/static/media/my_first_computer.c070c582.jpg"
+  },
+  {
+    "revision": "5a2e509acfefa3ff4ed59e52f6267dc3",
+    "url": "/portfolio/static/media/mysql.5a2e509a.png"
+  },
+  {
+    "revision": "13fd6f34b3848138f7e28ac358b3a0ce",
+    "url": "/portfolio/static/media/p5hatty.13fd6f34.ttf"
+  },
+  {
+    "revision": "d54ff11c5368d3a64150f4e430ceec68",
+    "url": "/portfolio/static/media/persona5.d54ff11c.jpg"
+  },
+  {
+    "revision": "7cb2e4439d35463ceaf14cbec1dd62ee",
+    "url": "/portfolio/static/media/persona5_2.7cb2e443.png"
+  },
+  {
+    "revision": "394d14c9386a6f39ec9deb2e64910825",
+    "url": "/portfolio/static/media/persona5_calendar.394d14c9.png"
+  },
+  {
+    "revision": "10033dbdacb1cea5918da50f52250a40",
+    "url": "/portfolio/static/media/persona5_menu_3.10033dbd.jpg"
+  },
+  {
+    "revision": "3ce34b5cead29d64b3e102679fc5f431",
+    "url": "/portfolio/static/media/persona5_pattern.3ce34b5c.jpg"
+  },
+  {
+    "revision": "fed832f8a7a6e9395aacbfa1eed1b86e",
+    "url": "/portfolio/static/media/php.fed832f8.png"
+  },
+  {
+    "revision": "768f937c81e43ddfb330cfaf56814c92",
+    "url": "/portfolio/static/media/racingjs.768f937c.png"
+  },
+  {
+    "revision": "a69125ed9f0fe50e62e513d8b620b0a2",
+    "url": "/portfolio/static/media/reactjs.a69125ed.png"
+  },
+  {
+    "revision": "31093aeab3cb9b5373c675359d62ca58",
+    "url": "/portfolio/static/media/secondyear.31093aea.png"
+  },
+  {
+    "revision": "fc5be1799a5e7abd66c6f20503288490",
+    "url": "/portfolio/static/media/symfony.fc5be179.png"
+  }
+]);
