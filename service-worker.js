c411bc8a1f27b7faa1ae34e9ad42108fcbaf9021
Yoann Menard <yoann.menard@epitech.eu>
2020-09-15 16:02:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.09a742606e502d7223f7966bb3a19c1e.js"
+  "/portfolio/precache-manifest.238a37d8523078d88e321e0716b519e0.js"
 );
 
 self.addEventListener('message', (event) => {
